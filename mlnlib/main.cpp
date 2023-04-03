75418891917aaf4a04b9db03b0e0c79f2772ee21
Cedric Hirschi <cedr02@live.com>
2023-04-03 11:40:16 +0200

    Added initial Microchip studio files

---
+++ mlnlib/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * mlnlib.cpp
+ *
+ * Created: 03.04.2023 11:13:26
+ * Author : cedr0
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
